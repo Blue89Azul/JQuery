8863405b969bff14e0d007543413b52610082553
Aoi <blue89azul@gmail.com>
2020-06-16 06:05:48 +0000

    ２回目修正版

--- test01.js
+++ test01.js
@@ -1,81 +1,78 @@
-// //課題１・２
-// var answer = prompt("正の値か負の値を入れてください。", "");
+//課題１・２
+var answer = prompt("正の値か負の値を入れてください。", "");
 
 
-// if(answer > 0){
-//     alert("正の値です");
-// } else if (answer < 0){
-//     alert("負の値です");
-// } else if (answer == 0) {
-//     alert("ゼロです");
-// } else {
-//     alert("数値を入力してください");
-// }
+if(answer > 0){
+    alert("正の値です");
+} else if (answer < 0){
+    alert("負の値です");
+} else if (answer == 0) {
+    alert("ゼロです");
+} else {
+    alert("数値を入力してください");
+}
 
-// //課題３
+//課題３
 
-// var animals = ["パンダ", "ライオン", "キリン"];
-// var i = 0;
+var animals = ["パンダ", "ライオン", "キリン"];
+var i = 0;
 
-// while (i < animals.length){
-//     alert(animals[i]);
-//     i++;
-// }
+while (i < animals.length){
+    alert(animals[i]);
+    i++;
+}
 
 
 //課題４、５
 var answer = prompt("パンダ、ライオン、キリンのどれが好きですか？")
 
 var animals = ["パンダ", "ライオン", "キリン"];
-var i = 0;
 
-    while (i < animals.length){
+
+for(var i = 0; i < animals.length; i++ ) {
     
-    if(answer == "ライオン"){
+        if(answer == "ライオン"){
         
-        alert("ライオンです。添字は" + "[" + animals.indexOf("ライオン") + "]");
-    
+        alert( answer + "[" + animals.indexOf(answer) + "]");
         break;
-        
-    } else {
+    } 
+    
         alert(animals[i]);
-        i++;
-    }
     
 }
 
 
-// //課題６,７
-// 　var animals =[
-// 　    {"name": "パンダ","weight": 80},
-// 　    {"name": "ライオン","weight": 200},
-// 　    {"name": "キリン","weight": 300}
-// 　    ];
+//課題６,７
+　var animals =[
+　    {"name": "パンダ","weight": 80},
+　    {"name": "ライオン","weight": 200},
+　    {"name": "キリン","weight": 300}
+　    ];
 
-// var answer = prompt("パンダ、ライオン、キリンのどれかを入力してください");
+var answer = prompt("パンダ、ライオン、キリンのどれかを入力してください");
 
-// switch (answer) {
-//     case 'パンダ':
-//         alert("この動物の体重は" + animals[0].weight + "kgです。")
-//         break;
-//     case 'ライオン':
-//         alert("この動物の体重は" + animals[1].weight + "kgです。")
-//         break;
-//     case 'キリン':
-//         alert("この動物の体重は" + animals[2].weight + "kgです。")
-//         break;
+switch (answer) {
+    case 'パンダ':
+        alert("この動物の体重は" + animals[0].weight + "kgです。")
+        break;
+    case 'ライオン':
+        alert("この動物の体重は" + animals[1].weight + "kgです。")
+        break;
+    case 'キリン':
+        alert("この動物の体重は" + animals[2].weight + "kgです。")
+        break;
     
-//     default:
-//         alert("別の動物です！")
-// }
+    default:
+        alert("別の動物です！")
+}
 
-// // 課題7
+// 課題7
 
-// function circle(num) {
-//     return num * num * 3.14;
-// }
+function circle(num) {
+    return num * num * 3.14;
+}
 
-// alert(circle(2));
+alert(circle(2));
 
 
 
--- test02.js
+++ test02.js
@@ -1,10 +1,35 @@
 (function($, window) {
     $(function() {
         
-    $('h1').addClass('title');
-    $('#index').removeClass('list');
+// 課題１
+$('h2').remove();
+
+ 
+// 課題２:alertメソッドで確認。
+alert($('#index').text());
+// テキストのみが出力（テキストのみ変更できる）
+alert($('#index').html());
+//htmlタグも出力（htmlタグも解釈して出力できる）
+
+
+//課題３
+$('#index').append('<li>メソッド</li>')
+
+
+//課題4
+$("#index").find('li').each(function() {
     
-    $('h2').show();
-    $('#index').find('li').eq(0).hide();
+    alert($(this).text().length);
+    
+});
+
+
+//課題5
+$("#index").find('li').each(function() {
+    
+$(this).text().after($(this).html($(this).text().length + "文字"));
+    
+});
+
     });
 })(jQuery, window);
