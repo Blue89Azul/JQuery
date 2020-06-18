00017723f278930a7114c5e60739213e2bbfb198
Aoi <blue89azul@gmail.com>
2020-06-18 02:38:39 +0000

    複数改行無し

--- test02.js
+++ test02.js
@@ -4,17 +4,14 @@
         // 課題１
         $('h2').remove();
 
-
         // 課題２:alertメソッドで確認。
         alert($('#index').text());
         // テキストのみが出力（テキストのみ変更可能）
         alert($('#index').html());
         //htmlタグも出力（htmlタグも解釈して出力可能）
 
-
         //課題３
         $('#index').append('<li>メソッド</li>')
-
 
         //課題4
         $("#index").find('li').each(function() {
@@ -23,14 +20,12 @@
 
         });
 
-
         //課題5
         $("#index").find('li').each(function() {
 
             $(this).append("<span>[" + $(this).text().length + "文字]</span>");
 
         });
-
 
     });
 })(jQuery, window);
